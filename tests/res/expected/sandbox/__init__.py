5dbbdfc60d2694b7ffdcc39cccdc9111c244f425
ccp_zeulix <thordurm@ccpgames.com>
2024-04-15 15:41:37 +0000

    Version 5.0.0-beta.1 - Going live
    - Migrated over

--- tests/res/expected/sandbox/__init__.py
+++ tests/res/expected/sandbox/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.1.0'
-__protoplasm_version__ = '5.0.0-beta.2'
-__neobuilder_version__ = '4.7.0'
+__protoplasm_version__ = '5.0.0'
+__neobuilder_version__ = '5.0.0-beta.1'
 
 
 def load_symbols():
